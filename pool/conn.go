462a3b01a604c5f9965f4bdadc0100e2640f70d7
lazystarstar <starichat@163.com>
2022-05-24 11:48:15 +0800

    feat: 优化连接池代码

--- pool/conn.go
+++ pool/conn.go
@@ -3,17 +3,15 @@ package pool
 import (
 	"context"
 	"google.golang.org/grpc"
-	"sync"
-	"sync/atomic"
+	"google.golang.org/grpc/connectivity"
 )
 
 //ConnState 连接状态分为
 type ConnState int
 
 const (
-	Idle       ConnState = iota + 1 //空闲
-	Running                         //运行，正在使用
-	Busying                         //忙碌，暂不可用
+	UnAvaliable = 0
+	Avaliable
 )
 
 //Conn 一个连接大概有如下功能：
@@ -21,78 +19,39 @@ const (
 //3. 连接状态变更
 type Conn interface {
 	Value() *grpc.ClientConn //对应的类型需要用interface{}来断言
-	Close() error//关闭连接
-	UpdateState(targetState ConnState) error //连接的状态变更
-	Release() ConnState //释放连接
-	AddConnStream() ConnState //增加连接
-	Status() ConnState //当前连接状态
+	Close() error            //关闭连接
+	Status() ConnState       //当前连接状态
 }
 
 type GrpcConnWrap struct {
 	*grpc.ClientConn //集成grpc连接
-	State ConnState
-	mu sync.Mutex
-	ConnStreamCount int64 //当前连接数量
-	maxSize int64
+	State            ConnState
 }
 
 func (c *GrpcConnWrap) Status() ConnState {
+	switch c.GetState() {
+	case connectivity.TransientFailure, connectivity.Shutdown:
+		return UnAvaliable
+	default:
+		return Avaliable
+	}
 	return c.State
 }
 
-
-
 //DialGrpcConn 新建一个grpc连接
 func DialGrpcConn(addr string) (*GrpcConnWrap, error) {
-	cc, err := grpc.DialContext(context.Background(),addr,grpc.WithInsecure())
+	cc, err := grpc.DialContext(context.Background(), addr, grpc.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
-	return &GrpcConnWrap{ClientConn:cc,State: Idle,mu:sync.Mutex{},
-	maxSize: 10000}, nil
+	return &GrpcConnWrap{ClientConn: cc, State: Avaliable}, nil
 }
-
-
 
 //Close 关闭连接
 func (c *GrpcConnWrap) Close() error {
 	return c.ClientConn.Close()
 }
 
-
-
-//UpdateState 更新连接状态,连接更新操作需要加锁
-//ready -> idle
-//idle -> ready
-//idle -> busying
-func (c *GrpcConnWrap)	UpdateState(targetState ConnState) error {//连接的状态变更
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	if c.ConnStreamCount < c.maxSize {
-
-	}
-
-	return nil
-}
-
-func (c *GrpcConnWrap) Release() ConnState {
-	atomic.AddInt64(&c.ConnStreamCount,-1)
-	c.State = Running
-
-	return c.State
-}
-
 func (c *GrpcConnWrap) Value() *grpc.ClientConn {
 	return c.ClientConn
-}
-
-func (c *GrpcConnWrap) AddConnStream() ConnState {
-
-	atomic.AddInt64(&c.ConnStreamCount,1)
-	if c.ConnStreamCount < c.maxSize {
-		c.State = Running
-	} else {
-		c.State = Busying
-	}
-	return c.State
 }
--- pool/pool.go
+++ pool/pool.go
@@ -1,164 +1,84 @@
 package pool
 
 import (
-<<<<<<< HEAD
-	"container/ring"
-	"context"
-=======
-	"container/list"
->>>>>>> 11fb87368dcbcb9ed92c7343b0887db5c89c7f29
-	"errors"
-	"fmt"
-	"log"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
-<<<<<<< HEAD
 /**
 连接池，主要实现了如下功能：
-1. 连接的自动扩容和缩容算法
-2. 连接的调度
+1. 闲置连接释放
+2. 连接不够时扩容
+3. 连接全满时等待
 简单来说需要两个对象：
 1. 连接池结构
 2. 连接结构
 */
 
-
-
 type Pool struct {
-
-	MaxIdle int //最大空闲连接
-	MaxActive int //最大活跃连接
-	IdleTimeout time.Duration //todo
-	Wait bool //如果当前连接池，没有空闲连接了，就会可用，整个池子就进入等待状态了。这里可以用chan来处理，还可以规避并发问题
-
-	MaxConnLifetime time.Duration //连接最长生命周期
-
-	mu     sync.Mutex    // mu protects the following fields
-	closed bool          // set to true when the pool is closed.
-	active int           // the number of open connections in the pool
-	ch     chan struct{} // limits open connections when p.Wait is true
-	conns  *ring.Ring     // idle connections
-	idle *ring.Ring //idle 指向同一个环，只是索引位置不同
-
+	capacity int64
+	next     int64
+	addr   string
+	DialTimeout      time.Duration
+	KeepAlive        time.Duration
+	KeepAliveTimeout time.Duration
+	ClientPoolSize   int
+	lock sync.Mutex
+	conns []Conn
 }
 
-type Option func(Pool *Pool)
-
-var DefaultPool = &Pool{
-	MaxIdle:         10,
-	MaxActive:       10,
-	IdleTimeout:     0,
-	Wait:            false,
-	MaxConnLifetime: 0,
-	mu:              sync.Mutex{},
-	closed:          false,
-	active:          0,
-	ch:              make(chan struct{}),
-	idle:            []Conn,
-=======
-type Pool interface {
-	Get() (Conn, error) //获取连接
-	Put(i Conn) error //放置连接
-	Remove(i Conn) error //移除连接
-	Close()  //关闭连接池
->>>>>>> 11fb87368dcbcb9ed92c7343b0887db5c89c7f29
+type ConnPool interface {
+	Get() (Conn, error)
+	Put(i Conn, idx int64) error
+	Close()
 }
 
-/**
-如果当前连接busy，则将其移到尾部
-*/
-
-func (p *listPool)Get() (Conn, error) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	//从空闲池中获取数据
-	if p.idleConns != nil  {
-		for item := p.idleConns.Front();item != nil;item = item.Next(){
-			if cc := item.Value.(Conn); cc.Status() == Running || cc.Status() == Idle {
-				state := cc.AddConnStream()
-				if state == Busying {
-					p.idleConns.MoveToBack(item)
-				}
-				return cc, nil
-			}
-			fmt.Println(errors.New("busying"))
-			continue
-
-		}
-	}
-	return nil, errors.New("no conn")
-
-}
-
-func (p *listPool) Put(i Conn) error {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	if p.idleConns != nil {
-		if _, ok := p.conns[i];ok{
-			//该连接已经存在了，直接return
-			return errors.New("conn already exist")
-		}
-		fmt.Println("put a conn")
-		el := p.idleConns.PushFront(i)
-		p.conns[i] = el
-		return nil
-	}
-	return errors.New("no pool")
-}
-
-func (p *listPool) Close() {
-	fmt.Println("close todo ")
-}
-
-func (p *listPool) Remove(i Conn) error {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	if i != nil {
-		p.idleConns.Remove(p.conns[i])
-		delete(p.conns, i)
-	}
-	return nil
-}
-
-func (p *listPool) AutoResize() error {
-	//空闲比率小于一定阈值
-	if float32(p.idleConns.Len()) / float32(len(p.conns)) < 0.9 {
-		//todo 缩容
-	}
-	return nil
-}
-
-
-
-type listPool struct {
-	mu *sync.Mutex
-	idleConns *list.List
-	conns map[Conn]*list.Element
-	maxCurrentConn int
-}
-
-func InitPool(size int) Pool {
-	p := &listPool{
-		mu: &sync.Mutex{},
-		idleConns: list.New(),
-		conns:     make(map[Conn]*list.Element,0),
-	}
-	//初始化连接
-	for i:=0;i<size;i++{
-		wc, err := DialGrpcConn("192.168.3.3:10010")
-		if err != nil {
-			log.Println("err",err)
-			continue
-		}
-		err = p.Put(wc)
-		if err != nil {
-			panic(err)
-		}
-	}
+func NewConnPool(p *Pool) ConnPool {
 	return p
 }
 
+func (p *Pool) Get() (Conn, error) {
+	next := atomic.AddInt64(&p.next, 1)
+	idx := next % p.capacity
+	//1. 获取索引位置，取出连接
+	conn := p.conns[idx]
+	//2. 判断当前连接是否可用
+	if conn != nil {
+		if conn.Status() != UnAvaliable{
+			//3. 链接不可用，删除链接
+			conn.Close()
+		} else {
+			return conn, nil
+		}
+	}
 
+	//4. 创建新的链接,需要加锁
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	cc, err := DialGrpcConn(p.addr)
+	if err != nil {
+		return nil, err
+	}
+	p.conns[idx] = cc
+	return cc, nil
+}
+
+func (p *Pool) Put(c Conn, idx int64) error {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	p.conns[idx] = c
+	return nil
+}
+
+func (p *Pool) Close() {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	for _, v := range p.conns {
+		if v != nil {
+			v.Close()
+		}
+	}
+}
 
 
